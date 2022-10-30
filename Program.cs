0e9ae49753fef9cd35c73971be5c9ee01c4893be
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-30 23:40:14 +0530

    Add files via upload
    added bubble sort

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace BubbleSort_str
+{
+    public class bubbleSort
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 78, 55, 45, 98, 13 };
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted values are:");
+            foreach (int value in arr)
+            Console.Write(value + " ");
+            Console.Read();
+        }
+    }
+}
