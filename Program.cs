fc50e90cfd8131e3a308d1a4112c2dd13f73e46b
aneeshkatta <113498885+aneeshkatta@users.noreply.github.com>
2022-10-30 23:46:51 +0530

    Add files via upload
    added palidrome uc

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+namespace PalindromeCheck
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Console.Write("Enter a Number To Check Palindrome : ");
+                int number = int.Parse(Console.ReadLine());
+                int reminder, sum = 0;
+                int temp = number;
+                while (number > 0)
+                {   reminder = number % 10;
+                    sum = (sum * 10) + reminder;
+                    number = number / 10;
+                }
+                if (temp == sum)
+                {
+                    Console.WriteLine($"Number {temp} is Palindrome.");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {temp} is not Palindrome");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
